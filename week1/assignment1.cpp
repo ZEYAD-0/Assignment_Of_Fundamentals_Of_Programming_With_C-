733062ab1f0b6168cf4ce54d864779eaf00984eb
ZEYAD-0 <zeyadelganainy00@gmail.com>
2025-09-18 12:28:42 +0300

    Add files via upload

---
+++ week1/assignment1.cpp
@@ -0,0 +1,16 @@
+// This Is My First File
+// In Learning Programming
+
+
+#include <iostream> /* Preprocessor Directive */
+
+int main()
+{
+  std::cout // Character Output
+   << "Line One\n";
+  std::cout << "Line Two\n"; /* cout Function */
+  std::cout << "Line Three\n";
+  // Return 0
+  return 0;
+} /* End Main Function*/
+
